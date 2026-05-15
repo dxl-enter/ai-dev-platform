07f0bfe4650cf6935dfcf543507cf9f58f83cef0
dxl-enter <1579486716@qq.com>
2026-05-15 15:29:54 +0800

    Update ai-dev.js

--- bin/ai-dev.js
+++ bin/ai-dev.js
@@ -1,1 +1,17 @@
+#!/usr/bin/env node
 
+const path = require('path');
+const fs = require('fs');
+
+// 检查是否已构建
+const distPath = path.join(__dirname, '..', 'dist', 'index.js');
+
+if (fs.existsSync(distPath)) {
+  require(distPath);
+} else {
+  // 开发模式，使用ts-node
+  require('ts-node').register({
+    project: path.join(__dirname, '..', 'tsconfig.json')
+  });
+  require(path.join(__dirname, '..', 'src', 'index.ts'));
+}
